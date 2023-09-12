ce9284e0b2f4f235a24df6b8971df96574b43cfe
dungtientran <dungcq86@gmail.com>
2023-09-12 09:50:24 +0700

    fix trantranslations

--- src/admin/app.js
+++ src/admin/app.js
@@ -14,23 +14,54 @@ const config = {
   },
   locales: [
     'vi',
+    'fr'
   ],
   translations: {
 
     en: {
+
+      category: 'Category management',
+      layout: 'Theme management',
+      User: 'Customer management',
       "Auth.form.welcome.title": "Metadata Solutions Admin",
       "Auth.form.welcome.subtitle": "Log in to your account",
 
       "app.components.LeftMenu.navbrand.title": "Metadata Dashboard",
       "app.components.LeftMenu.navbrand.workplace": "Workplace",
-    },
-    vi: {
-      "Auth.form.welcome.title": "Metadata Solutions Admin",
-      "Auth.form.welcome.subtitle": "Đăng nhập với tài khoản",
+      "Media Library": "Hehheeh",
 
-      "app.components.LeftMenu.navbrand.title": "Metadata Dashboard",
-      "app.components.LeftMenu.navbrand.workplace": "Workplace",
+      "content-manager.containers.ListPage.table-headers.category_name": 'Categories',
+
+      'content-manager.containers.ListPage.table-headers.theme_name': 'Name theme',
+
     },
+
+    vi: {
+      category: 'Quản lý danh mục',
+      layout: 'Quản lý giao diện',
+      User: 'Quản lý khách hàng',
+      CATEGORY_NAME: 'Tên danh mục',
+
+      "Content Manager": "Content Manager sssssssssssssssssssssssssssssssssssssssssssss",
+      "content-manager.containers.SettingViewModel.pluginHeader.title": "Nội Dung",
+
+      "content-manager.containers.ListPage.table-headers.category_name": 'Tên danh mục',
+      'content-manager.containers.ListPage.table-headers.createdAt': 'Thời gian tạo',
+      'content-manager.containers.ListPage.table-headers.updatedAt': 'Thời gian cập nhật',
+
+      'content-manager.containers.ListPage.table-headers.theme_name': 'Tên giao diện',
+      'content-manager.containers.ListPage.table-headers.image': 'Ảnh',
+      'content-manager.containers.ListPage.table-headers.updatedBy': 'Người tạo',
+      'content-manager.containers.ListPage.table-headers.categories': 'Danh mục',
+      'content-manager.containers.ListPage.table-headers.state': 'Trạng thái',
+      'content-manager.containers.ListPage.table-headers.[Content available in]': 'Ngôn ngữ',
+      'content-manager.containers.ListPage.table-headers.phone_number': 'phone',
+    },
+
+    fr: {
+      "Media Library": "Thư viện",
+
+    }
 
   },
 
